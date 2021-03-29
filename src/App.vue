4c29235bc0a0a315350f27c54c1d1edc9ed644cf
Tim Hinz <timhinz16@gmail.com>
2021-03-28 19:02:56 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import "assets/scss/app";
+</style>
+<script>
+import Navbar from "src/components/Navbar";
+export default {
+  components: { Navbar },
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="navbar">
+    <ul>
+      <li>
+        <router-link to="/">Dashboard</router-link>
+      </li>
+
+      <li>
+        <router-link to="/">Dashboard</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped lang="scss">
+@import "../assets/scss/components/Navbar";
+</style>
